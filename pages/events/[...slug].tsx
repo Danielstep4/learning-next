2c0609d2e2f41729cfc2bd56056ff13b74a46924
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-03-17 15:29:18 +0200

    Done... shallow work very limited. just to experience next.js

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -4,17 +4,29 @@ import {
   DataFilter,
   DummyEvent,
 } from "../../data/DUMMY_DATA";
+import Event from "../../components/Event";
 
 const SingleEventWithQuery: React.FC = (): JSX.Element => {
   const router = useRouter();
   let filter: DataFilter;
   const { slug } = router.query;
+  if (!slug || !slug.length) return <h1>404</h1>;
   filter = {
     year: +slug[0],
     month: +slug[1],
   };
-  const chosenEvent: DummyEvent[] = getFilteredEvents(filter);
-  return <div>Single Event With Query</div>;
+  const chosenEvents: DummyEvent[] = getFilteredEvents(filter);
+  if (!chosenEvents.length) return <h1>Not Found</h1>;
+  const showData = chosenEvents.map((event, index) => (
+    <Event chosenEvent={event} key={index} />
+  ));
+
+  return (
+    <>
+      <div>Single Event With Query</div>
+      <div>{showData}</div>
+    </>
+  );
 };
 
 export default SingleEventWithQuery;
